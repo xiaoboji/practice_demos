41e37cfa9ccebcf326e61e30bdc7fa205aa85c53
xiaoboji <xiaoboji1990@gmail.com>
2020-06-26 00:06:15 +0800

    [xiaoboji][update][java-core][object methods]

--- java-core/src/v1ch05/equals/EqualsTest.java
+++ java-core/src/v1ch05/equals/EqualsTest.java
@@ -5,9 +5,28 @@ package v1ch05.equals;
  * @description: This program demonstrates the equals method
  * @author: xiaoboji
  * @create: 2020-06-25 22:35
- **/
+ */
 public class EqualsTest {
-    public static void main(String[] args) {
-        
-    }
+  public static void main(String[] args) {
+    Employee alice1 = new Employee("Alice Adams", 75000, 1987, 12, 15);
+    Employee alice2 = alice1;
+    Employee alice3 = new Employee("Alice Adams", 75000, 1987, 12, 15);
+    Employee bob = new Employee("Bob Brandson", 50000, 1989, 10, 1);
+    System.out.println("alice1 == alice2:" + (alice1 == alice2));
+    System.out.println("alice1 == alice3:" + (alice1 == alice3));
+    System.out.println("alice1.equals(alice3):" + alice1.equals(alice3));
+    System.out.println("alice1.equals(bob):" + alice1.equals(bob));
+    System.out.println("bob.toString():" + bob);
+
+    Manager car1 = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+    Manager boss = new Manager("Carl Cracker", 80000, 1987, 12, 15);
+
+    boss.setBonus(50000);
+    System.out.println("boss.toString(): " + boss);
+    System.out.println("carl.equals(boss): " + car1.equals(boss));
+    System.out.println("alicel.hashCode()： " + alice1.hashCode());
+    System.out.println("alice3.hashCode(): " + alice3.hashCode());
+    System.out.println("bob.hashCode(): " + bob.hashCode());
+    System.out.println("carl.hashCode(): " + car1.hashCode());
+  }
 }

fc82dabe5be1a5fb7dfb902b413abf740091e903
xiaoboji <xiaoboji1990@gmail.com>
2020-06-25 23:18:46 +0800

    [xiaoboji][update][java-core][object methods]

---
+++ java-core/src/v1ch05/equals/EqualsTest.java
@@ -0,0 +1,13 @@
+package v1ch05.equals;
+
+/**
+ * @program: practice-demos
+ * @description: This program demonstrates the equals method
+ * @author: xiaoboji
+ * @create: 2020-06-25 22:35
+ **/
+public class EqualsTest {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ java-core/src/v1ch05/equals/Manager.java
@@ -0,0 +1,14 @@
+package v1ch05.equals;
+
+/**
+ * @program: Manager
+ * @description: Manager Class
+ * @author: xiaoboji
+ * @create: 2020-06-25 22:37
+ **/
+public class Manager extends Employee {
+
+  public Manager(String name, double salary, int year, int month, int day) {
+    super(name, salary, year, month, day);
+  }
+}

80d7d2e96eababd2f4791fc86affe30b7c4967c7
xiaoboji <xiaoboji1990@gmail.com>
2020-07-12 23:23:57 +0800

    [xiaoboji][create][java-core][stackTrace]

---
+++ java-core/src/v1ch07/stackTrace/StackTraceTest.java
@@ -0,0 +1,35 @@
+package v1ch07.stackTrace;
+
+import java.util.Scanner;
+
+/**
+ * @program: practice-demos
+ * @description: A program that displays a trace feature fo a recursive method call.
+ * @author: xiaoboji
+ * @create: 2020-07-12 23:11
+ */
+public class StackTraceTest {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter n: ");
+    int n = in.nextInt();
+    factorial(n);
+  }
+
+  public static int factorial(int n) {
+    System.out.println("factorial(" + n + "):");
+    Throwable t = new Throwable();
+    StackTraceElement[] frames = t.getStackTrace();
+    for (StackTraceElement f : frames) {
+      System.out.println(f);
+    }
+    int r;
+    if (n <= 1) {
+      r = 1;
+    } else {
+      r = n * factorial(n - 1);
+    }
+    System.out.println("return " + r);
+    return r;
+  }
+}

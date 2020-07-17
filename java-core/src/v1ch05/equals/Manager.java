106abbd5ce24386ccd8936cd0ac6eb7398c0dd75
xiaoboji <xiaoboji1990@gmail.com>
2020-07-17 23:22:44 +0800

    [xiaoboji][create][java-core][pair3]

--- java-core/src/v1ch05/equals/Manager.java
+++ java-core/src/v1ch05/equals/Manager.java
@@ -21,6 +21,10 @@ public class Manager extends Employee {
     return baseSalary + bonus;
   }
 
+  public double getBonus() {
+    return bonus;
+  }
+
   public void setBonus(double bonus) {
     this.bonus = bonus;
   }

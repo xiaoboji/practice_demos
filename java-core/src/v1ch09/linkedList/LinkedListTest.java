5296d8dfddce34ba4b04910fcb92b6909ebb82a6
xiaoboji <xiaoboji1990@gmail.com>
2020-07-20 23:12:33 +0800

    [xiaoboji][create][java-core][linkedListTest]

---
+++ java-core/src/v1ch09/linkedList/LinkedListTest.java
@@ -0,0 +1,11 @@
+package v1ch09.linkedList;
+
+/**
+ * @program: practice-demos
+ * @description: This program demonstrates operations on linked lists
+ * @author: xiaoboji
+ * @create: 2020-07-20 23:11
+ **/
+public class LinkedListTest {
+
+}

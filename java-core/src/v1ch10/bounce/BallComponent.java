3a1f525eed598a15f9c4b604bd4bb11d98832091
xiaoboji <xiaoboji1990@gmail.com>
2020-07-27 23:52:06 +0800

    [xiaoboji][create][java-core][Bounce]

---
+++ java-core/src/v1ch10/bounce/BallComponent.java
@@ -0,0 +1,38 @@
+package v1ch10.bounce;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+/**
+ * @program: practice-demos
+ * @description: The component that draws the balls.
+ * @author: xiaoboji
+ * @create: 2020-07-27 23:32
+ */
+public class BallComponent extends JPanel {
+  private static final int DEFAULT_WIDTH = 450;
+  private static final int DEFAULT_HEIGHT = 350;
+
+  private java.util.List<Ball> balls = new ArrayList<>();
+
+  public void add(Ball b) {
+    balls.add(b);
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D g2 = (Graphics2D) g;
+    for (Ball b : balls) {
+      g2.fill(b.getShape());
+    }
+  }
+
+  @Override
+  public Dimension getPreferredSize() {
+    return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+  }
+}
---
+++ java-core/src/v1ch10/bounce/Bounce.java
@@ -0,0 +1,64 @@
+package v1ch10.bounce;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * @program: practice-demos
+ * @description: shows an animated bouncing ball.
+ * @author: xiaoboji
+ * @create: 2020-07-27 23:37
+ */
+public class Bounce {
+  public static void main(String[] args) {
+    EventQueue.invokeLater(
+        () -> {
+          JFrame frame = new BounceFrame();
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.setVisible(true);
+        });
+  }
+}
+
+class BounceFrame extends JFrame {
+  private BallComponent comp;
+  public static final int STEPS = 1000;
+  public static final int DELAY = 3;
+
+  public BounceFrame() {
+    setTitle("Bonuce");
+    comp = new BallComponent();
+    add(comp, BorderLayout.CENTER);
+    JPanel buttonpanel = new JPanel();
+    addButton(buttonpanel, "Start", event -> addBall());
+    addButton(buttonpanel, "Close", event -> addBall());
+    add(buttonpanel, BorderLayout.SOUTH);
+    pack();
+  }
+
+  public void addButton(Container c, String title, ActionListener listener) {
+    JButton button = new JButton(title);
+    c.add(button);
+    button.addActionListener(listener);
+  }
+
+  public void addBall() {
+    try {
+      Ball ball = new Ball();
+      comp.add(ball);
+
+      for (int i = 1; i < STEPS; i++) {
+        ball.move(comp.getBounds());
+        comp.paint(comp.getGraphics());
+        Thread.sleep(DELAY);
+      }
+    } catch (InterruptedException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+}

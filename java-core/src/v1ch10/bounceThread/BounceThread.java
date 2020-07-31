62dcf5c11a6b9632875ad4506e689f2d3f79b6ba
xiaoboji <xiaoboji1990@gmail.com>
2020-07-31 22:13:51 +0800

    [xiaoboji][create][java-core][BounceThread]

---
+++ java-core/src/v1ch10/bounceThread/BounceThread.java
@@ -0,0 +1,72 @@
+package v1ch10.bounceThread;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import v1ch10.bounce.Ball;
+import v1ch10.bounce.BallComponent;
+
+/**
+ * @program: practice-demos
+ * @description: show animated bouncing balls
+ * @author: xiaoboji
+ * @create: 2020-07-31 00:06
+ */
+public class BounceThread {
+  public static void main(String[] args) {
+    EventQueue.invokeLater(
+        () -> {
+          JFrame frame = new BounceFrame();
+          frame.setTitle("BounceThread");
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.setVisible(true);
+        });
+  }
+}
+
+class BounceFrame extends JFrame {
+  private BallComponent comp;
+  public static final int STEPS = 1000;
+  public static final int DELAY = 3;
+
+  public BounceFrame() {
+    setTitle("Bonuce");
+    comp = new BallComponent();
+    add(comp, BorderLayout.CENTER);
+    JPanel buttonpanel = new JPanel();
+    addButton(buttonpanel, "Start", event -> addBall());
+    addButton(buttonpanel, "Close", event -> addBall());
+    add(buttonpanel, BorderLayout.SOUTH);
+    pack();
+  }
+
+  public void addButton(Container c, String title, ActionListener listener) {
+    JButton button = new JButton(title);
+    c.add(button);
+    button.addActionListener(listener);
+  }
+
+  public void addBall() {
+    Ball ball = new Ball();
+    comp.add(ball);
+
+    Runnable r =
+        () -> {
+          try {
+            for (int i = 1; i < STEPS; i++) {
+              ball.move(comp.getBounds());
+              comp.repaint();
+              Thread.sleep(DELAY);
+            }
+          } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+          }
+        };
+    Thread t = new Thread(r);
+    t.start();
+  }
+}

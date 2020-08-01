88258b9c9280b9b714cadeb31e8031d2419a44e4
xiaoboji <xiaoboji1990@gmail.com>
2020-08-01 23:45:58 +0800

    [xiaoboji][create][java-core][UnsynchBankTest]

---
+++ java-core/src/v1ch10/unsynch/Bank.java
@@ -0,0 +1,66 @@
+package v1ch10.unsynch;
+
+import java.util.Arrays;
+
+/**
+ * The type Bank.
+ *
+ * @program: practice -demos
+ * @description: A bank with a number of bank accounts.
+ * @author: xiaoboji
+ * @create: 2020 -08-01 23:30
+ */
+public class Bank {
+  private final double[] accounts;
+
+  /**
+   * Instantiates a new Bank.
+   *
+   * @param n the n
+   * @param initialBalance the initial balance
+   */
+  public Bank(int n, double initialBalance) {
+    this.accounts = new double[n];
+    Arrays.fill(accounts, initialBalance);
+  }
+
+  /**
+   * Transfer.
+   *
+   * @param from the from
+   * @param to the to
+   * @param amount the amount
+   */
+  public void transfer(int from, int to, double amount) {
+    if (accounts[from] < amount) {
+      return;
+    }
+    System.out.print(Thread.currentThread());
+    accounts[from] -= amount;
+    System.out.printf(" %10.2f from %d to %d ", amount, from, to);
+    accounts[to] += amount;
+    System.out.printf("Total Balance : %10.2f%n", getTotalBalance());
+  }
+
+  /**
+   * Get total balance double.
+   *
+   * @return the double
+   */
+  public double getTotalBalance() {
+    double sum = 0;
+    for (double a : accounts) {
+      sum += a;
+    }
+    return sum;
+  }
+
+  /**
+   * Size int.
+   *
+   * @return the int
+   */
+  public int size() {
+    return accounts.length;
+  }
+}
---
+++ java-core/src/v1ch10/unsynch/UnsynchBankTest.java
@@ -0,0 +1,50 @@
+package v1ch10.unsynch;
+
+/**
+ * The type Unsynch bank test.
+ *
+ * @program: practice -demos
+ * @description: this program shows data corruption when multiple threads access a data structure
+ * @author: xiaoboji
+ * @create: 2020 -08-01 23:27
+ */
+public class UnsynchBankTest {
+  /** The constant NACCOUTS. */
+  public static final int NACCOUTS = 100;
+
+  /** The constant INITIAL_BALANCE. */
+  public static final double INITIAL_BALANCE = 1000;
+
+  /** The constant MAX_AMOUNT. */
+  public static final double MAX_AMOUNT = 1000;
+
+  /** The constant DELAY. */
+  public static final int DELAY = 10;
+
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    Bank bank = new Bank(NACCOUTS, INITIAL_BALANCE);
+    for (int i = 0; i < NACCOUTS; i++) {
+      int fromAccount = i;
+      Runnable r =
+          () -> {
+            try {
+              while (true) {
+                int toAccount = (int) (bank.size() * Math.random());
+                double amount = MAX_AMOUNT * Math.random();
+                bank.transfer(fromAccount, toAccount, amount);
+                Thread.sleep((int) (DELAY * Math.random()));
+              }
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          };
+      Thread t = new Thread(r);
+      t.start();
+    }
+  }
+}
